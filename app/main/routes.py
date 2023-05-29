03386c8045ff3445cf2814039c908d02055dd5cb
AlexJJGreen <alexjjgreen@gmail.com>
2023-05-29 14:36:36 +0200

    dynamic routing for blog content

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,22 @@
 from flask import render_template
+from app.models import Content
+from app import db
 from . import bp
+
+
+@bp.errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html"), 404
+
 
 @bp.route("/")
 @bp.route("/index")
 def index():
-    return render_template("index.html")
+    content = Content.query.all()
+    return render_template("index.html", content=content)
+
+
+@bp.route("/content/<int:id>", methods=["GET", "POST"])
+def content(id):
+    post = db.session.query(Content).get(id)
+    return render_template("content.html", post=post)
