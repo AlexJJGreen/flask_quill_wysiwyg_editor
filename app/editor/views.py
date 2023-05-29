f2bb55f6143649255f44ebda8a7eb82bfb64a42f
AlexJJGreen <alexjjgreen@gmail.com>
2023-05-29 13:44:54 +0200

    add backend post handler - session added to db

--- app/editor/views.py
+++ app/editor/views.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify, render_template
 from app import db
+from app.models import Content
 from . import bp
 
 
@@ -11,7 +12,14 @@ def editor():
 @bp.route("/post_content", methods=["POST"])
 def save_content():
     data = request.get_json()
-    # Save post_content to a database or a file.
-    # content = Content(content=form.)
-    print(data)
-    return jsonify({"data": data}), 200
+    content_to_post = Content(
+        title=data["title"],
+        snippet=data["snippet"],
+        thumbnail_url=data["thumbnailUrl"],
+        content=data["content"],
+    )
+
+    db.session.add(content_to_post)
+    db.session.commit()
+    print(data["title"])
+    return render_template("editor.html", page_title="Content Editor")
